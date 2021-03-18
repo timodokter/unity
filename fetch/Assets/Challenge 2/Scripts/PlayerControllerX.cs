3ca1d74251c8d78b2f60e9480b62fe0c71c1de01
Timo Dokter <timo.dokter@student.rocvf.nl>
2021-03-18 12:15:36 +0100

    challenge 2 is af

--- fetch/Assets/Challenge 2/Scripts/PlayerControllerX.cs
+++ fetch/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -6,13 +6,19 @@ public class PlayerControllerX : MonoBehaviour
 {
     public GameObject dogPrefab;
 
+    public float dogSpawnInterval = 0.0f;
+    
     // Update is called once per frame
     void Update()
     {
+
+        dogSpawnInterval++;
+        
         // On spacebar press, send dog
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && dogSpawnInterval > 200f)
         {
             Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            dogSpawnInterval = 0.0f;
         }
     }
 }
