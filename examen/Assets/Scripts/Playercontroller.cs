501ccb1d67915843abbfc24a5d0fceba97b81528
Timo Dokter <timo.dokter@student.rocvf.nl>
2021-05-10 09:01:18 +0200

    the script for the camera is working but not finished yet

--- examen/Assets/Scripts/Playercontroller.cs
+++ examen/Assets/Scripts/Playercontroller.cs
@@ -2,10 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Playercontroller : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
     public float SpeedH = 2;
     public float SpeedV = 2;
+
+    private float Horizontal = 0.0f;
+    private float Vertical = 0.0f;
     
     // Start is called before the first frame update
     void Start()
@@ -16,6 +19,9 @@ public class Playercontroller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Horizontal += SpeedH * Input.GetAxis("Mouse X");
+        Vertical -= SpeedV * Input.GetAxis("Mouse Y");
+
+        transform.eulerAngles = new Vector3(Vertical, Horizontal, 0.0f);
     }
 }
