9b2fc22c5da2837e8f0a4a14561a056947fedb24
Timo Dokter <timo.dokter@student.rocvf.nl>
2021-05-10 08:56:40 +0200

    some asset files

---
+++ examen/Assets/Scripts/Player controller.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playercontroller : MonoBehaviour
+{
+    public float SpeedH = 2;
+    public float SpeedV = 2;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
