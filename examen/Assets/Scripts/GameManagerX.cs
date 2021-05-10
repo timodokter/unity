f8c8c374f1b5b91a04cc798c6a546e05d1d65489
Timo Dokter <timo.dokter@student.rocvf.nl>
2021-05-10 11:34:22 +0200

    i made a file to to manage the game and check if its running

---
+++ examen/Assets/Scripts/GameManagerX.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManagerX : MonoBehaviour
+{
+    public bool isGameActive = false;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        isGameActive = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- examen/Assets/Scripts/Playercontroller.cs
+++ examen/Assets/Scripts/Playercontroller.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class PlayerController : MonoBehaviour
 {
+    //variables of the camera
     public float SpeedH = 4;
     public float SpeedV = 4;
 
@@ -19,11 +21,22 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //camera movement
         rotationY += SpeedV * Input.GetAxis("Mouse X");
         rotationX -= SpeedH * Input.GetAxis("Mouse Y");
 
         rotationX = Mathf.Clamp(rotationX, -30f, 30f);
         
         transform.eulerAngles = new Vector3(0, rotationY, rotationX);
+        shootGun();
+    }
+
+    private void shootGun()
+    {
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            
+            Debug.Log("left mouse button is pressed");
+        }
     }
 }
