2ee83983343c2b2204b1f7d2c74b765eb5c8bde3
Timo Dokter <timo.dokter@student.rocvf.nl>
2021-05-11 10:48:26 +0200

    the bullet spawn correctly but it does not have a velocity yet

--- examen/Assets/Scripts/Playercontroller.cs
+++ examen/Assets/Scripts/Playercontroller.cs
@@ -1,47 +1,78 @@
-using System.Collections;
-using System.Collections.Generic;
+using System.Numerics;
 using UnityEngine;
-using UnityEngine.UIElements;
+using Quaternion = UnityEngine.Quaternion;
+using Vector3 = UnityEngine.Vector3;
 
 public class PlayerController : MonoBehaviour
 {
-    // private GamemanagerX GamemanagerX;
+    private GamemanagerX GamemanagerX;
     
     //variables of the camera
-    public float SpeedH = 4;
-    public float SpeedV = 4;
+    public float speedH = 4;
+    public float speedV = 4;
 
-    private float rotationX = 0.0f;
-    private float rotationY = 0.0f;
+    private float rotationX;
+    private float rotationY;
     
+    //variables for the gun
+    public Transform BulletSpawnPosition;
+    
+    public GameObject BulletPrefab;
+
+    private float BulletVelocity = 5; 
+    private float fireTimer;
+    private float xRotationOfBullet, yRotationOfBullet, zRotationOfBullet, wRotationOfBullet;
+
+    private bool shot;
+
     // Start is called before the first frame update
     void Start()
     {
-        // GamemanagerX = GameObject.Find("GameManager").GetComponent<GamemanagerX>();
+        GamemanagerX = GameObject.Find("GameManager").GetComponent<GamemanagerX>();
+        shot = false;
     }
 
     // Update is called once per frame
     void Update()
     {
         //camera movement
-        rotationY += SpeedV * Input.GetAxis("Mouse X");
-        rotationX -= SpeedH * Input.GetAxis("Mouse Y");
+        rotationY += speedV * Input.GetAxis("Mouse X");
+        rotationX -= speedH * Input.GetAxis("Mouse Y");
 
         rotationX = Mathf.Clamp(rotationX, -30f, 30f);
         
         transform.eulerAngles = new Vector3(0, rotationY, rotationX);
-        shootGun();
+
+        if (shot)
+        {
+            fireTimer += Time.deltaTime;
+        }
+        if (fireTimer > 2f)
+        {
+            shot = false;
+        }
+        if (!shot)
+        {
+            ShootGun();
+        }
+        // Debug.Log(shot);
+        // Debug.Log(Mathf.Round(fireTimer));
+        // Debug.Log(xRotationOfBullet);
+        // Debug.Log(yRotationOfBullet);
+        // Debug.Log(zRotationOfBullet);
     }
 
-    private void shootGun()
+    private void ShootGun()
     {
-        // while (GamemanagerX.isGameActive)
-        // {
+        if (GamemanagerX.isGameActive)
+        {
             if (Input.GetKey(KeyCode.Mouse0))
             {
-
+                Instantiate(BulletPrefab, BulletSpawnPosition.position, BulletSpawnPosition.rotation);
+                shot = true;
+                fireTimer = 0;
                 Debug.Log("left mouse button is pressed");
             }
-        // }
+        }
     }
 }
