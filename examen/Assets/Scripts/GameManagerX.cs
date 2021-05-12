bc3c1140dcbc4168e1109071c8ee34ebe34fd060
Timo Dokter <timo.dokter@student.rocvf.nl>
2021-05-12 13:31:34 +0200

    the reload animations are working and there is a sound effect for shooting

--- examen/Assets/Scripts/GameManagerX.cs
+++ examen/Assets/Scripts/GameManagerX.cs
@@ -1,20 +1,31 @@
-using System.Collections;
-using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class GamemanagerX : MonoBehaviour
 {
-    public bool isGameActive = false;
+    private PlayerController _playerController;
     
+    public bool isGameActive;
+
+    public  TextMeshProUGUI ammoCountText;
+
     // Start is called before the first frame update
     void Start()
     {
+        _playerController = GameObject.FindWithTag("Player").GetComponent<PlayerController>();
         isGameActive = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+    }
+
+    public void UpdateAmmoCount()
+    {
+        ammoCountText.text = "AMMO" +
+                             "\nCurrent Magazine: " + _playerController.currAmmo + 
+                             "\nExtra Ammo: " + _playerController.extraAmmo;
     }
 }
--- examen/Assets/Scripts/Playercontroller.cs
+++ examen/Assets/Scripts/Playercontroller.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using Vector3 = UnityEngine.Vector3;
 
@@ -17,16 +18,27 @@ public class PlayerController : MonoBehaviour
     
     public GameObject BulletPrefab;
 
-    private float BulletVelocity = 30f; 
+    public float currAmmo = 30;
+    public float extraAmmo = 30;
+    public float bulletsShot;
+    private float BulletVelocity = 50f;
     private float fireTimer;
+    private float reloadSpeed = 1f;
 
     private bool shot;
+    private bool reloading;
+
+    private Animator weaponAnimator;
+
+    public AudioSource gunshotSound;
 
     // Start is called before the first frame update
     void Start()
     {
         GamemanagerX = GameObject.Find("GameManager").GetComponent<GamemanagerX>();
         shot = false;
+        weaponAnimator = GameObject.FindGameObjectWithTag("Weapon").GetComponent<Animator>();
+        Debug.Log(BulletVelocity);
     }
 
     // Update is called once per frame
@@ -55,7 +67,25 @@ public class PlayerController : MonoBehaviour
         Debug.Log(shot);
         Debug.Log(Mathf.Round(fireTimer));
         
-        
+        //reload animation
+        if (Input.GetKeyDown(KeyCode.R) && !reloading)
+        {
+            if (currAmmo == 30)
+            {
+                return;
+            }
+            if (extraAmmo >= 0)
+            {
+                StartCoroutine(Reload());
+                return;
+            }
+        }
+        if (currAmmo <= 0)
+        {
+            StartCoroutine(Reload());
+            return;
+        }
+        GamemanagerX.UpdateAmmoCount();
     }
 
     private void ShootGun()
@@ -68,11 +98,28 @@ public class PlayerController : MonoBehaviour
 
                 instantiatedBullet.GetComponent<Rigidbody>().velocity =
                     transform.TransformDirection(new Vector3(-BulletVelocity, 0, 0));
+                gunshotSound.Play();
                 
                 shot = true;
                 fireTimer = 0;
-                Debug.Log("left mouse button is pressed");
+                currAmmo--;
+                bulletsShot++;
+                Debug.Log("a bullet was shot");
             }
         }
+    }
+
+    IEnumerator Reload()
+    {
+        reloading = true;
+        weaponAnimator.SetBool("reloading", true);
+        
+        yield return new WaitForSeconds(reloadSpeed);
+
+        extraAmmo -= bulletsShot;
+        bulletsShot = 0;
+        currAmmo = 30;
+        reloading = false;
+        weaponAnimator.SetBool("reloading", false);
     }
 }
